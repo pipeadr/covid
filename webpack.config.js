3363a2dc319aa1d81ef4ba13b448db69a50a4915
pipeadr <delriopipe12@gmail.com>
2021-08-01 21:22:34 -0500

    project completion and webpack configuration start

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -9,4 +11,37 @@ module.exports = {
     resolve: {
         extensions: [".js"]
     },
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader
+                test: /\.js$/,
+                exclude: /node_modules/, // IGNORA LOS MODULOS DE LA CARPETA
+                // Use es un arreglo u objeto donde dices que loader aplicaras
+                use: {
+                  loader: "babel-loader"
+                },
+                // Exclude permite omitir archivos o carpetas especificas
+                exclude: /node_modules/
+              },
+              {
+                test: /\.css$/i,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  "css-loader",
+                ]
+              },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
+            // inject: true,
+            inject: 'body',
+            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+            filename: './index.html'// NOMBRE FINAL DEL ARCHIVO
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+    ],
 }
